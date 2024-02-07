c5e733afde90b589d4dcd30c27613cf6cc90dafd
EteshBarck <159249626+EteshBarck@users.noreply.github.com>
2024-02-07 16:56:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 document.getElementById('startGame').addEventListener('click', function() {
     var canvas = document.getElementById('stockGameCanvas');
     var ctx = canvas.getContext('2d');
-    
-    canvas.width = 400;
-    canvas.height = 200;
-    
-    // Basit bir dalgalanma çizgisi çizelim
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.moveTo(0, 100);
-    for (let i = 0; i < canvas.width; i+=10) {
-        let value = Math.random() * 100; // Rastgele bir yükseklik değeri
-        ctx.lineTo(i, 100 - value + Math.random() * 50);
+    ctx.moveTo(0, canvas.height / 2);
+    for (let i = 0; i < canvas.width; i += 10) {
+        let value = Math.random() * canvas.height;
+        ctx.lineTo(i, value);
     }
     ctx.stroke();
 });
